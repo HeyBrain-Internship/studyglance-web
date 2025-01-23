d779a54e724b3def5237cd7b7db4dba3254f55a8
Elektroair <elektroair.aero@gmail.com>
2025-01-23 01:46:44 +0000

    Just a fix

--- src/App.tsx
+++ src/App.tsx
@@ -24,6 +24,16 @@ function MainPage()
     const buttonStartClick = async () =>
     {
         setIsStartPage(false);
+        try
+        {
+            const response = await fetch("http://localhost:8080/get");
+            const data = await response.json();
+            console.log(data);
+        }
+        catch(error)
+        {
+            console.log(error);
+        }
     }
 
     return (
