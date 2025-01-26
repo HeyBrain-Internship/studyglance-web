b12cacb3245984569ff0471fe60d66c9f462d8dc
Elektroair <elektroair.aero@gmail.com>
2025-01-26 12:04:37 +0000

    Question script

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,7 @@ import { GoogleOAuthProvider } from '@react-oauth/google';
 import App from './App';
 
 
-const clientId = 'ВАШ_GOOGLE_CLIENT_ID';
+const clientId = 'CLIENT_ID';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
