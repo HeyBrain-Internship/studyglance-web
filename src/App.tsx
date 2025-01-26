a4d795712f6a4109f68a1bee44f8776e470527e0
Elektroair <elektroair.aero@gmail.com>
2025-01-26 11:39:05 +0000

    Question script

--- src/App.tsx
+++ src/App.tsx
@@ -62,7 +62,7 @@ function MainPage()
 
         try
         {
-            const response     = await fetch("http://127.0.0.1:8080/get/" + grade);
+            const response     = await fetch("http://studyglance.space/api/get-" + grade);
             const dataImported = await response.json();
             const newData      = dataImported["receivedData"];
             console.log("DATA 1", newData);
