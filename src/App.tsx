0307fa297521116561b464754cdc63785e437f63
Elektroair <elektroair.aero@gmail.com>
2025-01-28 08:29:32 +0000

    Fix of light theme

--- src/App.tsx
+++ src/App.tsx
@@ -62,8 +62,7 @@ function MainPage()
 
         try
         {
-            //const response     = await fetch("https://studyglance.space/api/get-" + grade);
-            const response     = await fetch("http://127.0.0.1:8080/get-" + grade);
+            const response     = await fetch("https://studyglance.space/api/get-" + grade);
             const dataImported = await response.json();
             const newData      = dataImported["receivedData"];
 
